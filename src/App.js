36b185a6d71d40a646f759e32bf2d10727a477a2
Kender <kenderromain@gmail.com>
2023-07-20 12:14:47 -0400

    Define my routes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import PropertyDetails from './pages/PropertyDetails';
 
 const App = () => {
-  return <div>react app</div>;
+  return <div>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/property/:id' element={<PropertyDetails/>} />
+    </Routes>
+    <Footer/>
+  </div>;
 };
 
 export default App;
